159ccb9728a914bedeaaab6f1dfdfe1cd4cb302b
DK\tpe <tpe@sitecore.net>
2019-06-04 17:18:47 +0300

    Inital commit

---
+++ src/Sitecore.Support.257316.334356/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.257316.334356")]
+[assembly: AssemblyProduct("Sitecore.Support.257316.334356")]
+[assembly: ComVisible(false)]
